786a6b7e3e025ec5d39baa3ae5268cb9bc626214
prescious101 <frederickalago@gmail.com>
2016-10-12 09:23:23 +0800

    Changes in Main Activity

--- app/src/main/java/com/usjr/finalsexam/activity/MainActivity.java
+++ app/src/main/java/com/usjr/finalsexam/activity/MainActivity.java
@@ -1,13 +1,22 @@
 package com.usjr.finalsexam.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import com.usjr.finalsexam.R;
+import com.usjr.finalsexam.adapters.VideoListAdapter;
+import com.usjr.finalsexam.entity.TestData;
+import com.usjr.finalsexam.entity.Video;
 import com.usjr.finalsexam.fragments.VideoListFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    TestData testData = new TestData();
+    Video video = new Video();
+    VideoListAdapter videoListAdapter   ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +26,14 @@ public class MainActivity extends AppCompatActivity {
         FragmentManager fm = getSupportFragmentManager();
         VideoListFragment videoListFragment =
                 (VideoListFragment) fm.findFragmentById(R.id.videoListFragment);
+
+
+
+        Fragment fragment = new VideoListFragment();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.videoListFragment,fragment);
+        fragmentTransaction.commit();
+
+
     }
 }
